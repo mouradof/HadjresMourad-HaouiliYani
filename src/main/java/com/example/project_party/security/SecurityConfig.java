b5185bdbe6a4d735908a20b730e8aa88851a075e
mouradof <mouradhadjresp@gmail.com>
2024-12-13 11:44:06 +0100

    login register with token

--- src/main/java/com/example/project_party/security/SecurityConfig.java
+++ src/main/java/com/example/project_party/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.project_party.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,24 +7,17 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
-
-    @Autowired
-    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/users/**").authenticated()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-        System.out.println("Security Filter Chain configured");
+                        .anyRequest().permitAll()
+                );
+        System.out.println("Security Filter Chain configured with no restrictions");
         return http.build();
     }
 
